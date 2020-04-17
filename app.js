cef05433f36bee517d1fb95b709f59bccbef3b49
unknown <spdsender@gmailcom>
2020-04-17 21:32:44 +0530

    Update listening port number.

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.get("/", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started at port 3000");
 });
